5beaa663d0136d4cce1035c3d6bf5e037b084a92
lionfish <devisserrik@gmail.com>
2026-07-25 21:37:54 +0200

    Refactor findUser function for improved clarity

--- script.js
+++ script.js
@@ -1,47 +1,68 @@
-async function findUser() {
+async function findUser(){
 
-    const username = document.getElementById("username").value;
-    const result = document.getElementById("result");
+const username = document.getElementById("username").value;
+const result = document.getElementById("result");
 
-    result.innerHTML = "Loading...";
+result.innerHTML = "Loading profile...";
 
-    try {
+try {
 
-        const userReq = await fetch(
-            "https://roblox-api.devisserrik.workers.dev/?username=" + username
-        );
+const res = await fetch(
+"https://roblox-api.devisserrik.workers.dev/?username=" + username
+);
 
-        const userData = await userReq.json();
+const data = await res.json();
 
-        if (!userData.data || userData.data.length === 0) {
-            result.innerHTML = "❌ User not found";
-            return;
-        }
 
-        const user = userData.data[0];
-
-        result.innerHTML = `
-            <h2>${user.displayName}</h2>
-
-            <p>Username: ${user.name}</p>
-            <p>User ID: ${user.id}</p>
-            <p>Verified: ${user.hasVerifiedBadge ? "Yes" : "No"}</p>
-
-            <button onclick="copyID('${user.id}')">
-                Copy User ID
-            </button>
-        `;
-
-    } catch(error) {
-
-        console.log(error);
-        result.innerHTML = "❌ Error loading user";
-
-    }
+if(!data.data || data.data.length === 0){
+    result.innerHTML = "❌ User not found";
+    return;
 }
 
 
-function copyID(id) {
-    navigator.clipboard.writeText(id);
-    alert("Copied User ID: " + id);
+const user = data.data[0];
+
+
+result.innerHTML = `
+
+<h2>${user.displayName}</h2>
+
+<p>👤 Username: ${user.name}</p>
+
+<p>🆔 User ID: ${user.id}</p>
+
+<p>✔️ Verified:
+${user.hasVerifiedBadge ? "Yes" : "No"}
+</p>
+
+<br>
+
+<a target="_blank" href="https://www.roblox.com/users/${user.id}/profile">
+Open Roblox Profile
+</a>
+
+<br><br>
+
+<button onclick="copyID('${user.id}')">
+Copy User ID
+</button>
+
+`;
+
+} catch(error){
+
+console.log(error);
+result.innerHTML = "❌ Error loading user";
+
+}
+
+}
+
+
+function copyID(id){
+
+navigator.clipboard.writeText(id);
+
+alert("Copied User ID: " + id);
+
 }
