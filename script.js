112b745f8db90c840fcf5811863f0444c50ceee3
lionfish <devisserrik@gmail.com>
2026-07-25 21:31:56 +0200

    Update script.js

--- script.js
+++ script.js
@@ -3,50 +3,28 @@ async function findUser() {
     const username = document.getElementById("username").value;
     const result = document.getElementById("result");
 
-    result.innerHTML = "Loading...";
+    result.innerHTML = "Testen...";
 
     try {
 
-        const userReq = await fetch(
+        const response = await fetch(
             "https://roblox-api.devisserrik.workers.dev/?username=" + username
         );
 
-        const userData = await userReq.json();
-
-        if (!userData.data || userData.data.length === 0) {
-            result.innerHTML = "❌ User bestaat niet";
-            return;
-        }
-
-        const user = userData.data[0];
+        const text = await response.text();
 
         result.innerHTML = `
-            <h2>${user.displayName}</h2>
-
-            <div class="info">
-                <p>👤 Username: ${user.name}</p>
-                <p>🆔 User ID: ${user.id}</p>
-                <p>✔️ Verified: ${user.hasVerifiedBadge ? "Ja" : "Nee"}</p>
-
-                <button onclick="copyID('${user.id}')">
-                    Copy User ID
-                </button>
-            </div>
+            <h3>Response:</h3>
+            <pre>${text}</pre>
         `;
 
-    } catch(error) {
+    } catch (error) {
+
+        result.innerHTML = `
+            ❌ Fetch fout:<br>
+            ${error}
+        `;
 
         console.log(error);
-        result.innerHTML = "❌ Error loading user";
-
     }
-}
-
-
-function copyID(id) {
-
-    navigator.clipboard.writeText(id);
-
-    alert("User ID gekopieerd: " + id);
-
 }
